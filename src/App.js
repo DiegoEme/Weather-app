df4234ac51fd78084f7f0df600440e39d3f21017
Diego <soterg.uhdeia@gmail.com>
2021-02-05 22:27:55 -0500

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
+const api_key = '&appid=73e48893d12c632c3b7f98b00786a353';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q='
+
 function App() {
+const [searchTerm, setSearchTerm] = useState('');
+const [weather, setWeather] = useState({});
+
+const searchWeather = async() => {
+  await fetch(url+searchTerm+api_key)
+  .then(res => res.json())
+  .then((data) => {
+    console.log(data)
+    setWeather(data)
+  })
+  setSearchTerm('')
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input 
+        value = {searchTerm}
+        onChange={e => setSearchTerm(e.target.value)} 
+        type="text" 
+        placeholder="Search..."/>
+        <button onClick={searchWeather}>Search</button>
+      </div>
     </div>
   );
 }
