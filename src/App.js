52ab78cbd40e86807b407cab826691cdbcc8bd32
Diego <soterg.uhdeia@gmail.com>
2021-02-07 22:36:46 -0500

    layout structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import { useState } from "react";
-import LineChart from './LineChart';
+import LineChart from "./LineChart";
 import "./App.css";
 
 const apiKey = "&lang=es&units=metric&appid=73e48893d12c632c3b7f98b00786a353"; //datos dia actual
 const url = "https://api.openweathermap.org/data/2.5/weather?q="; //url base
-const forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" //datos 5 dias siguientes
-
+const forecast = "https://api.openweathermap.org/data/2.5/forecast?q="; //datos 5 dias siguientes
 
 function App() {
-
   const [searchTerm, setSearchTerm] = useState("");
   const [weather, setWeather] = useState({});
-  const [sunrise, setSunrise] = useState("")
-  const [sunset, setSunset] = useState("")
-  const [forecastData, setForecastData] = useState({})
+  const [sunrise, setSunrise] = useState("");
+  const [sunset, setSunset] = useState("");
+  const [forecastData, setForecastData] = useState({});
 
   //searchWeather se ejecuta al hacer clink en buscar
   const searchWeather = async () => {
@@ -23,13 +21,13 @@ function App() {
       .then((data) => {
         //console.log(data);
         setWeather(data);
-        
+
         //Se establencen datos sunrise y sunset despues de hacer conversion de tiempos
         setSunrise(convertTime(data.sys.sunrise, data.timezone));
         setSunset(convertTime(data.sys.sunset, data.timezone));
       });
 
-      //Se borra la barra de busqueda
+    //Se borra la barra de busqueda
     setSearchTerm("");
 
     //Datos pronostico 5 dias siguientes
@@ -39,13 +37,10 @@ function App() {
         //console.log(data);             
         setForecastData(data)
       }); */
-    
- 
   };
 
-  
-  //Funcion que convierte los datos de sunrise y sunset 
-   function convertTime(unixTime, offset) {
+  //Funcion que convierte los datos de sunrise y sunset
+  function convertTime(unixTime, offset) {
     let dt = new Date((unixTime + offset) * 1000);
     let h = dt.getHours() + 5;
     let m = "0" + dt.getMinutes();
@@ -53,7 +48,6 @@ function App() {
     return t;
   }
 
-  
   return (
     <div className="App">
       <div className="searchBar">
@@ -67,30 +61,39 @@ function App() {
       </div>
 
       {typeof weather.main != "undefined" ? (
-        <div className="card">
-          <div className="card__body">
-            <h3 className="card__body--city">
-              {weather.name}, {weather.sys.country}
-            </h3>
-            <div className="date">{new Date().toDateString()}</div>
-            <h1 className="card__body--temp">
-              {Math.round(weather.main.temp)} °C
-            </h1>
-            <h2 className="card__body--desc">
-              {weather.weather[0].description}
-            </h2>
-            <p>max: {Math.round(weather.main.temp_max)} °C</p>
-            <p>min: {Math.round(weather.main.temp_min)} °C</p>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+            <div className="card ">
+              <div className="card__body">
+                <h3 className="card__body--city">
+                  {weather.name}, {weather.sys.country}
+                </h3>
+                <div className="date">{new Date().toDateString()}</div>
+                <h1 className="card__body--temp">
+                  {Math.round(weather.main.temp)} °C
+                </h1>
+                <h2 className="card__body--desc">
+                  {weather.weather[0].description}
+                </h2>
+                <p>max: {Math.round(weather.main.temp_max)} °C</p>
+                <p>min: {Math.round(weather.main.temp_min)} °C</p>
+              </div>
+              <div className="card__info">
+                <li>Viento {Math.round(weather.wind.speed)} km/h</li>
+                <li>Humedad {weather.main.humidity}%</li>
+                <li>Presión {weather.main.pressure} hPa</li>
+                <li>Salida de sol {sunrise}</li>
+                <li>Puesta de sol {sunset}</li>
+              </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="chart">
+              <LineChart term={searchTerm} />
+              </div>
+            </div>
           </div>
-          <div className="card__info">
-            <li>Viento {Math.round(weather.wind.speed)} km/h</li>
-            <li>Humedad {weather.main.humidity}%</li>
-            <li>Presión {weather.main.pressure} hPa</li>
-            <li>Salida de sol {sunrise}</li>
-            <li>Puesta de sol {sunset}</li>
-            
-          </div>
-          <LineChart term = {searchTerm} />
         </div>
       ) : (
         ""
--- src/LineChart.js
+++ src/LineChart.js
@@ -1,118 +1,91 @@
 import { Line } from "react-chartjs-2";
 import { useState, useEffect } from "react";
 
-const LineChart = ({term}) => {
-    const apiKey = "&lang=es&units=metric&appid=73e48893d12c632c3b7f98b00786a353"; //datos dia actual
-    const forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" //datos 5 dias siguientes
-     
-
-    //props.list = array(40)
+const LineChart = ({ term }) => {
+  const apiKey = "&lang=es&units=metric&appid=73e48893d12c632c3b7f98b00786a353"; //datos dia actual
+  const forecast = "https://api.openweathermap.org/data/2.5/forecast?q="; //datos 5 dias siguientes
 
   const [chartData, setChartData] = useState({});
-  const [list, setList] = useState([])  
-  const [forecastDataMax, setForecastDataMax] = useState([])
-  const [forecastDataMin, setForecastDataMin] = useState([])
-  
+
   let maxTemp = [];
   let minTemp = [];
 
-  const chart =  () => {
-   fetch(`${forecast}${term}${apiKey}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log('estosi',data.list);             
-      //setForecastData(data);
+  const chart = () => {
+    fetch(`${forecast}${term}${apiKey}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("estosi", data.list);
 
-      for(let i = 0; i <= 40 ; i+= 6){
-        maxTemp.push(data.list[i].main.temp_max);
-        minTemp.push(data.list[i].main.temp_min);
-        setForecastDataMax(maxTemp)
-        setForecastDataMin(minTemp)
-        console.log('compp', maxTemp, forecastDataMax)
-      } 
-      
+        for (let i = 0; i <= 40; i += 6) {
+          maxTemp.push(data.list[i].main.temp_max);
+          minTemp.push(data.list[i].main.temp_min);
+         
+        }
 
-      setChartData({
-        
-        labels: ['1', '2', '3', '4', '5'],
-        datasets: [{
-            label: 'Temperatura máxima',
-            data: maxTemp,
-            borderWidth: 4,
-            type: 'line',
-            fill: false,
-            borderColor: 'red'
-        }, {
-          label: 'Temperatura mínima',
-          data: minTemp,
-          borderWidth: 4,
-          type: 'line',
-          fill: false,
-          borderColor: 'blue'
-        }],
+        setChartData({
+          labels: ["1", "2", "3", "4", "5"],
+          datasets: [
+            {
+              label: "Temperatura máxima",
+              data: maxTemp,
+              borderWidth: 4,
+              type: "line",
+              fill: false,
+              borderColor: "red",
+              pointBorderWidth: 2
+            },
+            {
+              label: "Temperatura mínima",
+              data: minTemp,
+              borderWidth: 4,
+              type: "line",
+              fill: false,
+              borderColor: "blue",
+              pointBorderWidth: 2
+            },
+          ],
+        });
+
+        console.log("arrays", minTemp, maxTemp);
       });
-      
-      console.log('arrays', minTemp, maxTemp);
-    });
-    
-      let days = ['1', '2', '3', '4', '5'];
-    /*   let maxTemp = [];
-      let minTemp = [];
-
-       for(let i = 0; i <= 40 ; i+= 7){
-        maxTemp.push(props.list[i].main.temp_max);
-        minTemp.push(props.list[i].main.temp_min)
-      }  */
-      //console.log(maxTemp, minTemp)
-
-    /* setChartData({
-    
-      labels: ['1', '2', '3', '4', '5'],
-      datasets: [{
-          label: 'y',
-          data: maxTemp, minTemp,
-          //backgroundColor: 'aqua',
-          borderWidth: 4
-      }],
-    }); */
   };
 
   useEffect(() => {
-    chart()
-  }, [])
+    chart();
+  }, []);
 
-  return(
+  return (
     <div>
-        <div style= {{height: "300px", width: "600px"}}>
-            <Line data={chartData} options = {{
-                responsive: true,
-                title: {text: 'Pronóstico 5 días', display: true},
-                scales: {
-                    yAxes: [
-                        {
-                            ticks: {
-                                autoSkip: true,
-                                maxTicksLimit: 10,
-                            },
-                            gridLines: {
-                                display: false
-                            }
-                        }
-                    ],
-                    xAxes: [
-                        {
-                            gridLines: {
-                                display: false
-                            }
-                        }
-                    ]
-                }
-            }
-               
-            }/>
-        </div>
+      <div style={{ height: "300px", width: "400px", margin: "auto" }}>
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            title: { text: "Pronóstico 5 días", display: true },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 10,
+                  },
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+              xAxes: [
+                {
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+            },
+          }}
+        />
+      </div>
     </div>
-    
   );
 };
 
